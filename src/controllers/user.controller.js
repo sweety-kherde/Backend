6fc8fc7f722ad10cab4fd50c959fba418d0689aa
sweety-kherde <Sweety.kherde16@gmail.com>
2025-02-18 18:50:47 +0530

    add register controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,56 @@
 import { asyncHandler } from "../utils/asyncHandler.js"
+import { ApiError } from "../utils/ApiError.js"
+import { User } from "../models/user.model.js"
+import { uploadOnCloudinary } from "../utils/cloudinary.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
 const registerUser = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: "Ok"
   })
+  const { fullname, email, username, password } = req.body
+  console.log("email:", email);
+  if ([fullname, email, username, password].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "All fields are required")
+  }
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }]
+  })
+  if (existedUser) {
+    throw new ApiError(409, "Username already exist ")
+  }
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar file is required")
+  }
+  const avatar = await uploadOnCloudinary(avatarLocalPath)
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+  if (!avatar) {
+    throw new ApiError(400, "Avatar file is required")
+  }
+  const user = await User.create({
+    fullname,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase()
+  })
+  const createduser = await User.findById(user._id).select("-password -refreshToken")
+  if (!createduser) {
+    throw new ApiError(500, "Something went wrong while registering user")
+  }
+  return res.status(201).json(
+    new ApiResponse(200, createduser, "User Registered Successfully")
+  )
 })
 export { registerUser, }
+
+
+
+
+
+
 // export const registerUser = async (req, res) => {
 //   console.log("✅ registerUser function executed", req.body);
 //   res.status(200).json({ message: "User registration successful!" });
